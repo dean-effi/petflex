d16fc3398f1031cfd649de1a3257fa3b16074a25
Dean <deaneffi1414@gmail.com>
2024-09-23 22:53:12 +0300

    send new pet trough state, make input controlled

--- src/pages/PostPetPage.tsx
+++ src/pages/PostPetPage.tsx
@@ -1,23 +1,55 @@
 import { useMutation } from "@tanstack/react-query";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
 export default function PostPetPage() {
+  const [newPet, setNewPet] = useState({
+    name: "",
+    description: "",
+  });
+
+  console.log(newPet);
   const postMut = useMutation({
     mutationFn: (newPet: any) => postPet(newPet),
   });
 
+  function onInputChange(
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) {
+    setNewPet({
+      ...newPet,
+      [e.target.name]: e.target.value,
+    });
+  }
   function onFormSubmit(e: FormEvent) {
     console.log("form");
     e.preventDefault();
-    postMut.mutate({
-      name: "hello",
-    });
+    postMut.mutate(newPet);
   }
   return (
-    <form onSubmit={onFormSubmit}>
+    <form
+      onSubmit={onFormSubmit}
+      className="grid justify-start p-4 text-xl"
+    >
       <label>
         Name:
-        <input type="text" name="name" />
+        <input
+          value={newPet.name}
+          onChange={onInputChange}
+          className="border border-black"
+          type="text"
+          name="name"
+        />
+      </label>
+      <label>
+        description:
+        <textarea
+          value={newPet.description}
+          onChange={onInputChange}
+          className="border border-black"
+          name="description"
+        />
       </label>
       <button type="submit">Submit</button>
     </form>
@@ -34,6 +66,7 @@ async function postPet(newPet: any) {
     }
     const data = new FormData();
     data.append("name", newPet.name);
+    data.append("description", newPet.description);
     const response = await fetch(
       import.meta.env.VITE_ENDPOINT + "posts/fake",
       {
