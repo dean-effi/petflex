dab7e21dd1994dfb1637a43ee555aa92b82fa117
Dean <deaneffi1414@gmail.com>
2024-09-27 12:29:18 +0300

    decrease font sizes and gaps in user form

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -33,14 +33,14 @@ export default function UserForm({
     return;
   }
   return (
-    <main className="relative m-auto w-[300px] overflow-visible p-8 py-10 text-blue-800 sm:w-[400px] sm:text-xl md:text-2xl lg:w-[455px] lg:text-[28px]">
+    <main className="relative m-auto w-[300px] overflow-visible p-8 py-10 text-blue-800 sm:w-[400px] sm:text-xl lg:w-[455px] lg:text-xl">
       <section
         aria-label="header"
         className="header text-center font-extrabold"
       >
         <h1 className="grid">
           {formType === "signup" ? "Sign up to" : "Log in to"}
-          <span className="text-6xl tracking-widest sm:text-7xl">
+          <span className="text-5xl tracking-widest sm:text-6xl">
             {" "}
             petflex
           </span>
@@ -51,16 +51,16 @@ export default function UserForm({
           alt="petflex logo"
         />
       </section>
-      <section className="registeration mt-12 lg:mt-14">
+      <section className="registeration mt-10">
         <form
           action=""
           onSubmit={e => {
             e.preventDefault();
             onFormSubmit(userDetails);
           }}
-          className="mt-auto grid gap-5 sm:gap-6 lg:gap-7"
+          className="mt-auto grid gap-5"
         >
-          <label className="grid gap-3 font-bold sm:gap-4 lg:gap-5">
+          <label className="grid gap-3 font-bold sm:gap-4">
             Username:
             <input
               name="username"
@@ -72,7 +72,7 @@ export default function UserForm({
               className="rounded-lg bg-stone-300 p-2 font-normal text-stone-800"
             />
           </label>
-          <label className="grid gap-3 font-bold sm:gap-4 lg:gap-5">
+          <label className="grid gap-3 font-bold sm:gap-4">
             Password:
             <input
               name="password"
@@ -84,22 +84,24 @@ export default function UserForm({
               className="rounded-lg bg-stone-300 p-2 font-normal text-stone-800"
             />
           </label>
-          <div className="pb-2 text-center font-bold text-red-800 xl:text-2xl">
-            {errorMsg && <p className="">{errorMsg}</p>}
-          </div>
+          {errorMsg && (
+            <p className="text-center font-bold text-red-800">
+              {errorMsg}
+            </p>
+          )}
 
           <div className="m-auto grid w-full gap-4 text-center">
             <button
               type="submit"
               disabled={isPending}
-              className="m-auto rounded-[20px] bg-blue-800 p-2 px-5 text-xl font-bold text-stone-100 hover:bg-blue-950 active:bg-blue-600 disabled:bg-slate-700 sm:text-2xl md:text-[26px] lg:p-3 lg:px-6 lg:text-3xl lg:text-[28px]"
+              className="m-auto rounded-[20px] bg-blue-800 p-1.5 px-[14px] text-lg font-semibold text-stone-100 hover:bg-blue-950 active:bg-blue-600 disabled:bg-slate-700 sm:text-xl lg:text-2xl xl:mt-2 xl:p-2 xl:px-4"
             >
               {formType === "signup" ? "Sign up" : "Log in"}
             </button>
             {formType === "signup" ? (
-              <p className="text-sm md:text-base lg:text-xl xl:text-2xl">
+              <p className="text-sm md:text-base lg:text-xl">
                 Already have an account?
-                <Link to="/login" className="font-extrabold">
+                <Link to="/login" className="font-bold">
                   {" "}
                   Login
                 </Link>
@@ -107,7 +109,7 @@ export default function UserForm({
             ) : (
               <p>
                 Don't have an account?
-                <Link to={"/signup"} className="font-extrabold">
+                <Link to={"/signup"} className="font-bold">
                   {" "}
                   Sign up
                 </Link>
