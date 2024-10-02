4eea1c7bd6b83f4cc631ed2bec62cdf5f4aa45d8
Dean <deaneffi1414@gmail.com>
2024-10-02 15:34:43 +0300

    dont include default values in filtering url & requests to improve caching

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,24 +7,23 @@ import PostPreview from "../components/PostPreview";
 import { useSearchParams } from "react-router-dom";
 
 type QueryOptionsType = {
-  order: "-1" | "1" | null;
-  sortBy: string | null;
-  petType: petsType | "" | null;
+  order: "-1" | "1" | undefined;
+  sortBy: string | undefined;
+  petType: petsType | undefined;
 };
 
 export default function HomePage() {
   const [searchParams, setSearchParams] = useSearchParams();
-  console.log(searchParams.toString(), " searchinggggggg");
   const [queryOptions, setQueryOptions] = useState<QueryOptionsType>({
-    order: null,
-    sortBy: null,
-    petType: null,
+    order: undefined,
+    sortBy: undefined,
+    petType: undefined,
   });
 
   function handleInputChange(
     e: React.ChangeEvent<HTMLSelectElement>
   ) {
-    console.log(e.target.name);
+    console.log("value", e.target.value);
     setQueryOptions({
       ...queryOptions,
       [e.target.name]: e.target.value,
@@ -32,8 +31,6 @@ export default function HomePage() {
 
     return;
   }
-
-  console.log(queryOptions);
 
   const postsQuery = useInfiniteQuery<PostType[], QueryError>({
     queryKey: ["posts", searchParams.toString()],
@@ -63,12 +60,26 @@ export default function HomePage() {
 
   function handleFiltersSubmit(e: FormEvent) {
     e.preventDefault();
-    console.log("submit");
-    setSearchParams(
-      `order=${queryOptions.order || "-1"}&sortBy=${queryOptions.sortBy || "date"}&petType=${queryOptions.petType || ""}`
-    );
+    console.log(queryOptions);
+    const params: Record<string, string> = {};
+    let param: keyof QueryOptionsType;
+    for (param in queryOptions) {
+      const assign = queryOptions[param];
+      if (
+        assign != undefined &&
+        typeof assign == "string" &&
+        !["-1", "all", "date"].includes(assign)
+      ) {
+        params[param] = assign;
+      }
+    }
+    const search = new URLSearchParams(params);
+    console.log(search);
+    setSearchParams(params);
   }
+
   const postsDisplay: ReactElement[] = [];
+
   if (pages) {
     pages.forEach(page =>
       page.forEach(post => {
@@ -93,7 +104,7 @@ export default function HomePage() {
             value={
               queryOptions.sortBy ||
               searchParams.get("sortBy") ||
-              "date"
+              undefined
             }
             onChange={handleInputChange}
           >
@@ -104,16 +115,15 @@ export default function HomePage() {
         <label>
           Pet Type:
           <select
-            defaultChecked={true}
             name="petType"
             onChange={handleInputChange}
             value={
               queryOptions.petType ||
               searchParams.get("petType") ||
-              ""
+              undefined
             }
           >
-            <option value="">all</option>
+            <option value="all">all</option>
             <option value="dog">dog</option>
             <option value="cat">cat</option>
             <option value="rabbit">rabbit</option>
@@ -127,12 +137,14 @@ export default function HomePage() {
           <select
             name="order"
             value={
-              queryOptions.order || searchParams.get("order") || "-1"
+              queryOptions.order ||
+              searchParams.get("order") ||
+              undefined
             }
             onChange={handleInputChange}
           >
-            <option value={-1}>descending</option>
-            <option value={1}>ascending</option>
+            <option value={-1}>descending!!</option>
+            <option value={1}>ascending!!!!!!!1</option>
           </select>
         </label>
         <button
