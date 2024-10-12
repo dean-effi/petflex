693f09b3a257a0cf528cbdae4163c5879a128ae2
Dean <deaneffi1414@gmail.com>
2024-10-13 01:02:58 +0300

    style navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import { NavLink } from "react-router-dom";
 import burger from "../assets/burger.svg";
-import burgerBlue from "../assets/burger-blue.svg";
 
 import moon from "../assets/moon.svg";
-import moonBlue from "../assets/moon-blue.svg";
+
+import logo from "../assets/logo.svg";
 
 import { queryClient } from "../main";
 import { useContext, useRef, useState } from "react";
@@ -28,20 +28,20 @@ export default function Navbar() {
         : " nav-close";
 
   return (
-    <div className="sticky top-0 z-10 flex gap-4 bg-blue-800 text-stone-100 shadow-md shadow-blue-200">
+    <div className="sticky top-0 z-10 flex gap-4 bg-gradient-to-b from-stone-50 to-stone-200 text-violet-800 shadow-[0px_3px_3px] shadow-violet-300">
       <button
         onClick={() => {
           if (!everOpened.current) everOpened.current = true;
           setIsOpen(!isOpen);
         }}
-        className="rounded-md py-1 hover:bg-blue-900 lg:hidden"
+        className="ml-2 rounded-md py-1 hover:bg-stone-300 lg:hidden"
       >
         <img src={burger} alt="open menu" />
       </button>
       {/* small screen collapseable Nav */}
       <nav
         className={
-          "absolute left-0 grid h-[100vh] w-[40%] min-w-[270px] grid-rows-[100px,1fr,100px] justify-between border-r bg-stone-100 p-2 text-blue-800 shadow-lg lg:hidden" +
+          "absolute left-0 grid h-[100vh] w-[40%] min-w-[270px] grid-rows-[100px,1fr,100px] justify-between border-r bg-stone-100 p-2 shadow-lg lg:hidden" +
           smallNavStateClass
         }
       >
@@ -50,9 +50,9 @@ export default function Navbar() {
             e.stopPropagation();
             setIsOpen(!isOpen);
           }}
-          className="self-start justify-self-start rounded-md hover:bg-stone-400"
+          className="self-start justify-self-start rounded-md hover:bg-stone-300"
         >
-          <img src={burgerBlue} alt="close menu " />
+          <img src={burger} alt="close menu " />
         </button>
         <ul className="flex flex-col gap-6 p-4 pt-0 text-2xl font-bold md:gap-8 md:text-3xl">
           <NavbarLinks
@@ -62,26 +62,19 @@ export default function Navbar() {
           />
         </ul>
         <button className="self-start p-4 pt-0">
-          <img
-            src={moonBlue}
-            className="w-10 md:w-12"
-            alt="moon icon"
-          />
+          <img src={moon} className="w-10 md:w-12" alt="moon icon" />
         </button>
       </nav>
       {/* large menu */}
-      <nav className="hidden w-full justify-between p-4 px-6 lg:flex">
-        <NavLink
-          className={"text-3xl font-bold tracking-widest"}
-          to={""}
-        >
-          petflex
+      <nav className="hidden w-full items-center justify-between p-4 px-6 lg:flex xl:px-8 2xl:px-9">
+        <NavLink to={""}>
+          <img src={logo} />
         </NavLink>
-        <ul className="flex items-center gap-4 text-2xl font-semibold">
+        <ul className="flex items-center gap-8 text-2xl font-bold">
           <NavbarLinks user={user} logout={logout} />
 
           <button>
-            <img src={moon} alt="moon icon" />
+            <img className="h-[32px]" src={moon} alt="moon icon" />
           </button>
         </ul>
       </nav>
